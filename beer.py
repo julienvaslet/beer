54f3e5d575876ca87bc25cdfe0101a034454b605
Julien Vaslet <julien.vaslet@gmail.com>
2017-02-03 19:33:10 +0100

    Added banner

--- beer.py
+++ beer.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+def banner():
+	print( "" )
+	print( "╻ ╻   ┏┓ ┏━╸┏━╸┏━┓   ╻" )
+	print( "╺╋╸   ┣┻┓┣╸ ┣╸ ┣┳┛   ╹" )
+	print( "╹ ╹   ┗━┛┗━╸┗━╸╹┗╸   ╹" )
+	print( "╺━━━━━━━━━━━━━━━━━━━━╸" )
+	
+
+if __name__ == "__main__":
+	banner()
+	
