693f36a1a1cf9e0f0cacf6282cb9930bff17df04
Julien Vaslet <julien.vaslet@gmail.com>
2017-02-04 17:25:04 +0100

    added empty summary

--- beer.py
+++ beer.py
@@ -6,8 +6,13 @@ from units import *
 from shell import Shell
 
 if __name__ == "__main__":
+	# Banner and summary
 	banner()
-	
 	shell = Shell( "beer" )
+	
+	shell.print( "Recipes: %d" % 0, lpad=3 )
+	shell.print( "Malts: %d" % 0, lpad=3 )
+	shell.print( "Hops: %d\n" % 0, lpad=3 )
+	
 	shell.run()
 	#a = unit.Unit()
