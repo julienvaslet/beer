323586f94a2b336236519046e9d8f79743e82113
Julien Vaslet <julien.vaslet@gmail.com>
2017-02-20 11:25:30 +0100

    Added language documentation

--- language.py
+++ language.py
@@ -6,10 +6,28 @@ import errno
 import configparser
 
 class Language():
+	"""Represents the singleton Language class which provides localized texts.
+	
+	Represents the singleton Language class which provides localized texts.
+	Text values are stored in INI files and can be load with the `load` method.
+	By default, files are located in "<app root>/i18n/<language>/" directory.
+	INI's section names are part of the full key-path of a text. For instance,
+	this class, Language, is fully named "language.Language". In our fictive
+	INI file there will be a "language.Language" section with a key "my_text".
+	To access this value, the language `get` method will be used by passing
+	the class and the key: `Language.get( Language, "my_text" )`. The path-key
+	will be generated as "language.Language.my_text" and will match our INI
+	parsed file.
+	
+	Class variables:
+		- (Language) _instance: the current Language instance.
+	"""
 
 	_instance = None
 
 	def __init__( self, lang, path ):
+		"""Internal Language instance initialization."""
+		
 		self._language = lang
 		self._path = path
 		self._content = {}
@@ -17,6 +35,16 @@ class Language():
 	
 	@classmethod
 	def initialize( cls, lang="en", path=None ):
+		"""Initialize the language instance.
+		
+		Initialize the language instance. If the path is unspecified
+		the default used path is "<app root>/i18n".
+		
+		Parameters:
+			- lang: The language (default: "en").
+			- path: The languages files location (default: None).
+		"""
+		
 		if path == None:
 			path = os.path.dirname( os.path.abspath( sys.argv[0] ) ) + os.sep + "i18n"
 			
@@ -25,6 +53,8 @@ class Language():
 	
 	@classmethod
 	def getInstance( cls ):
+		"""Returns the current Language instance (initialize it if necessary)."""
+		
 		if cls._instance == None:
 			cls.initialize()
 	
@@ -33,11 +63,15 @@ class Language():
 	
 	@classmethod
 	def getPath( cls, class_, key ):
+		"""Returns the full key-path according to class and key values."""
+		
 		return class_.__module__ + "." + class_.__qualname__ + "." + key
 	
 	
 	@classmethod
 	def load( cls, filename ):
+		"""Loads an INI file into the Language text collection."""
+		
 		language = cls.getInstance()
 		
 		if os.path.isfile( language._path + os.sep + language._language + os.sep + filename ):
@@ -54,9 +88,17 @@ class Language():
 	
 	@classmethod
 	def get( cls, class_, key ):
+		"""Returns the text-value according to the specified class_ and key values.
+		
+		Parameters:
+			- (class) class_: The class.
+			- (str) key: The text key.
+		"""
+		
 		language = cls.getInstance()
 		
 		if cls.getPath( class_, key ) in language._content:
 			return language._content[cls.getPath( class_, key )]
 		else:
 			return ""
+
