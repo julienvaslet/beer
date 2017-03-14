6e92e241ec173f32e5f0fa89cf7b556f13301e2f
Julien Vaslet <julien.vaslet@gmail.com>
2017-03-14 18:12:01 +0100

    added system autocompletion

--- beer.py
+++ beer.py
@@ -11,18 +11,34 @@ from units import *
 
 if __name__ == "__main__":
 
-	# Development needs
-	log.verbosity = 3
-	
-	Language.initialize( lang="en" )
-	Language.load( "hops.ini" )
-	Language.load( "ingredients.ini" )
-	ingredient.Ingredient.load_directory( "data%singredients" % os.sep )
-
-	argv = sys.argv
+	argv = list(sys.argv)
 	argv.pop( 0 )
 	
-	shell = beershell.BeerShell( verbosity=log.verbosity )
-	log.current_shell = shell
+	if len(argv) > 0 and argv[0] == "autocomplete":
+		argv.pop( 0 )
 	
-	shell.run( argv )
+		if len(argv) > 0:
+			Language.initialize( lang="en" )
+			Language.load( "hops.ini" )
+			Language.load( "ingredients.ini" )
+			ingredient.Ingredient.load_directory( "data%singredients" % os.sep )
+	
+			shell = beershell.BeerShell( verbosity=0 )
+			choices = shell.autocomplete( argv[0] )
+		
+		if len(choices):
+			print( "\n".join( choices ) )
+	
+	else:
+		# Development needs
+		log.verbosity = 3
+	
+		Language.initialize( lang="en" )
+		Language.load( "hops.ini" )
+		Language.load( "ingredients.ini" )
+		ingredient.Ingredient.load_directory( "data%singredients" % os.sep )
+	
+		shell = beershell.BeerShell( verbosity=log.verbosity )
+		log.current_shell = shell
+	
+		shell.run( argv )
